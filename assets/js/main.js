4c53953bbc73ba30a19410e49cfa9cf2a28c2cb1
andika purnama putra <andikapratamaputra1210@gmail.com>
2023-10-30 18:09:36 +0700

    --build: responsive section, animation, and component: --Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,56 @@
 console.log("hallo");
 
-// logic respondsive
+const btnRes = document.querySelector(".nav-btn-responsive");
+const menu = document.querySelector(".nav-menu");
+const nav = document.querySelector(".nav");
 
-// const btnRes = document.querySelector(".nav-btn-respondsive");
-// const menu = document.querySelector(".nav-menu");
+const toggleMenu = () => {
+  menu.classList.toggle("active");
+  btnRes.classList.toggle("cross");
+  nav.classList.toggle("bg-color");
+};
 
-// btnRes.addEventListener("click", () => {
-// 	menu.classList.toggle("active");
-// 	btnRes.classList.toggle("cross");
-// });
+const screenMedium = window.matchMedia("(max-width: 768px)");
+
+const handleMediaChange = (e) => {
+  if (e.matches) {
+    btnRes.addEventListener("click", toggleMenu);
+  } else {
+    btnRes.removeEventListener("click", toggleMenu);
+    menu.classList.remove("active");
+    btnRes.classList.remove("cross");
+    nav.classList.remove("bg-color");
+  }
+};
+
+handleMediaChange(screenMedium); // Memastikan responsive sesuai saat halaman dimuat
+
+screenMedium.addEventListener("change", handleMediaChange);
 
 // scroll navbar
 
 window.onscroll = function () {
-	scrollFunction();
+  scrollFunction();
 };
 
 function scrollFunction() {
-	if (
-		document.body.scrollTop > 80 ||
-		document.documentElement.scrollTop > 80
-	) {
-		document.getElementById("navbar").classList.add("bg-color");
-	} else {
-		document.getElementById("navbar").classList.remove("bg-color");
-	}
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").classList.add("bg-color");
+  } else {
+    document.getElementById("navbar").classList.remove("bg-color");
+  }
 }
 
 AOS.init({
-	disable: function () {
-		var maxWidth = 800;
-		return window.innerWidth < maxWidth;
-	},
-	duration: 1200,
-	easing: "ease-in-out", // default easing for AOS animations
-	once: true, // whether animation should happen only once - while scrolling down
-	mirror: true, // whether elements should animate out while scrolling past them
-	anchorPlacement: "top-bottom",
+  disable: function () {
+    var maxWidth = 800;
+    return window.innerWidth < maxWidth;
+  },
+  duration: 1200,
+  easing: "ease-in-out", // default easing for AOS animations
+  once: true, // whether animation should happen only once - while scrolling down
+  mirror: true, // whether elements should animate out while scrolling past them
+  anchorPlacement: "top-bottom",
 });
 
 // logic copy clipboard
@@ -47,29 +61,29 @@ const copyButtonLabel = "Components";
 let blocks = document.querySelectorAll("pre");
 
 blocks.forEach((block) => {
-	// only add button if browser supports Clipboard API
-	if (navigator.clipboard) {
-		let button = document.createElement("button");
+  // only add button if browser supports Clipboard API
+  if (navigator.clipboard) {
+    let button = document.createElement("button");
 
-		button.innerText = copyButtonLabel;
-		block.appendChild(button);
+    button.innerText = copyButtonLabel;
+    block.appendChild(button);
 
-		button.addEventListener("click", async () => {
-			await copyCode(block, button);
-		});
-	}
+    button.addEventListener("click", async () => {
+      await copyCode(block, button);
+    });
+  }
 });
 
 async function copyCode(block, button) {
-	let code = block.querySelector("code");
-	let text = code.innerText;
+  let code = block.querySelector("code");
+  let text = code.innerText;
 
-	await navigator.clipboard.writeText(text);
+  await navigator.clipboard.writeText(text);
 
-	// visual feedback that task is completed
-	button.innerText = "Code Copied!";
+  // visual feedback that task is completed
+  button.innerText = "Code Copied!";
 
-	setTimeout(() => {
-		button.innerText = copyButtonLabel;
-	}, 700);
+  setTimeout(() => {
+    button.innerText = copyButtonLabel;
+  }, 700);
 }
